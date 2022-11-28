678b0cde6037cd4bfff4089d7cbacca0989d1dac
DDimov <ddimov@jackhenry.com>
2022-11-28 18:03:58 +0200

    pushvam direktno

--- LVMasterAutomationDemo/Pages/Admin/ProcessFlow/ReportDefinitionPage.cs
+++ LVMasterAutomationDemo/Pages/Admin/ProcessFlow/ReportDefinitionPage.cs
@@ -21,11 +21,12 @@ namespace LVPages.Pages.Admin.ProcessFlow
         private IWebElement BuilderTab => driver.FindElement(By.CssSelector("span[data-ui='report-definitions-tab-item-title-builder']"));
         private IWebElement AddBuildReportButton => driver.FindElement(By.XPath("//button[contains(.,'Add/Build Report')]"));
         private IWebElement BuilderModalComponentsTab => driver.FindElement(By.CssSelector("span[data-ui='report-builder-components-tab-title']"));
+        private IWebElement AddAllComponentsButton => driver.FindElement(By.CssSelector("button[data-ui='report-builder-assign-all']"));
         private IList<IWebElement> PreConditionsItems => driver.FindElements
             (By.XPath("//div[contains(@id,'lv-droppable-Prerequisite Conditions')]//div[@class='lv-draggable-item']")).ToList();
         private IWebElement AssignedItemArea => driver.FindElement(By.CssSelector("#lv-droppable-assigned-drop-area"));
-        private IWebElement AssignedItemAreaXpath => driver.FindElement
-            (By.XPath("//*[text()='Drop items here.']"));
+        private IWebElement AssignedItems => driver.FindElement
+            (By.CssSelector("#lv-droppable-assigned-drop-area div[class='lv-draggable-item']"));
         private IWebElement AssignedItem => driver.FindElement
             (By.XPath("//div[@class='lv-header-right-section']//button"));
 
@@ -38,14 +39,53 @@ namespace LVPages.Pages.Admin.ProcessFlow
             ISeeElements(By.CssSelector("#reportsGrid tr"));
         }
 
+        public IList<string> Components()
+        {
+            List<string> componentslist = new List<string>()
+            {
+                "Prerequisite Conditions",
+                "Covenants",
+                "Relationship Information",
+                "Other",
+                "Policy Exceptions",
+                "Guarantors",
+                "Loans Summary",
+                "Recurring Documents",
+                "Beneficial Ownership",
+                "Recommendations"
+            };
+            return componentslist.AsReadOnly();
+        }
+
+        public void AddComponentsFromEachSection(IList<string> sections)
+        {
+            foreach (var item in sections)
+            {
+                var section = driver.FindElement(By.XPath("//span[contains(.,'" + sections + "')]"));
+                I.Click(section);
+                I.Click(AddAllComponentsButton);
+                ISeeElements(By.CssSelector("#lv-droppable-assigned-drop-area div[class='lv-draggable-item']"));
+            }
+        }
+
+        public void AddComponentsFromEachSection2(IList<string> Components)
+        {
+            foreach (var item in Components)
+            {
+                var section = driver.FindElement(By.XPath("//span[contains(.,'" + item + "')]"));
+                I.Click(section);
+                I.Click(AddAllComponentsButton);
+                ISeeElements(By.CssSelector("#lv-droppable-assigned-drop-area div[class='lv-draggable-item']"));
+            }
+        }
+
         public void AddBuildReport()
         {
             I.Click(BuilderTab);
+            Wait.ForLoaderToDissaper();
             I.Click(AddBuildReportButton);
             I.Click(BuilderModalComponentsTab);
-            ISeeElements(By.XPath("//div[contains(@id,'lv-droppable-Prerequisite Conditions')]//div[@class='lv-draggable-item']"));
-            IDragAndDrop(PreConditionsItems[0], AssignedItemArea);
-            IDragAndDrop(PreConditionsItems[1], AssignedItemAreaXpath);
+            AddComponentsFromEachSection2(Components());
         }
 
 
--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -14,7 +14,7 @@ namespace LVTests
         {
             driver = new ChromeDriver();
             PageHelper.PageBuilder(driver);
-            driver.Manage().Window.Size = new System.Drawing.Size(1440, 1080);
+            driver.Manage().Window.Size = new System.Drawing.Size(1440, 900);
             driver.Manage().Cookies.DeleteAllCookies();
             PageHelper.LoginPage.ClearCache();
         }
@@ -41,12 +41,12 @@ namespace LVTests
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
-            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
-            {
-                var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-                screenshot.SaveAsFile(@"C:\Users\Denislav\source\repos\LVMasterAutomationDemo\FailingTests\Screenshot.jpg"
-                , ScreenshotImageFormat.Jpeg);
-            }
+            //if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            //{
+            //    var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            //    screenshot.SaveAsFile(@"C:\Users\Denislav\source\repos\LVMasterAutomationDemo\FailingTests\Screenshot.jpg"
+            //    , ScreenshotImageFormat.Jpeg);
+            //}
 
             driver.Manage().Cookies.DeleteAllCookies();
             if (driver == null)
